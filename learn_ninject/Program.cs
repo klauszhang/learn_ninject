9adf37fbe105a8b36b1842e1c18dd353875bab6a
Hao Zhang <klaushaozhang.note@gmail.com>
2015-10-29 16:58:11 +1300

    apply ninject

--- learn_ninject/Program.cs
+++ learn_ninject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ninject;
 
 namespace learn_ninject
 {
@@ -10,6 +11,11 @@ namespace learn_ninject
   {
     static void Main(string[] args)
     {
+      using (var kernel=new Ninject.StandardKernel())
+      {
+        var service = kernel.Get<SalutationService>();
+        service.SayHello();
+      }
     }
   }
 }
